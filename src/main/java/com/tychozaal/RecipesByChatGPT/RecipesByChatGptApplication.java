da4e3422abe5dab4d7a14b4edbd278456d9a4dc4
NASA-PC\Tycho <tychozaal@gmail.com>
2023-04-17 19:01:10 +0200

    Project Setup

---
+++ src/main/java/com/tychozaal/RecipesByChatGPT/RecipesByChatGptApplication.java
@@ -0,0 +1,13 @@
+package com.tychozaal.RecipesByChatGPT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesByChatGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipesByChatGptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tychozaal/RecipesByChatGPT/RecipesByChatGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tychozaal.RecipesByChatGPT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesByChatGptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
